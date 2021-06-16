322e537797fab5195d445697a5020883a375453d
Carlos Jose Jimenez <cjimenez@deckprismsports.com>
2021-06-15 21:16:51 -0600

    Change styles for consistency.

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,7 +13,7 @@ const Contact: React.FC<PageProps> = (props: PageProps) => {
         <div className="mt-12 mb-4 mx-auto w-1/2 text-center text-red-800">
           <h1 className="text-4xl font-bold">Contact</h1>
         </div>
-        <p className="text-gray-700 mx-auto text-base font-body font-light leading-relaxed mb-6">
+        <p className="text-gray-700 mx-auto text-base font-body leading-relaxed mb-6">
           I am particularly versed in asp.net core backends and SPA front ends
           (mainly with React and Blazor). I also have experience in modern cloud
           applications and architecture, particularly with AWS. If you need to
@@ -22,14 +22,14 @@ const Contact: React.FC<PageProps> = (props: PageProps) => {
           modern web, then feel free to fill the information below and we can
           chat more about your project and how can I help.
         </p>
-        <ul className="mb-6 text-gray-700 text-base font-body font-light leading-relaxed list-disc list-inside ml-6">
+        <ul className="mb-6 text-gray-700 text-base font-body leading-relaxed list-disc list-inside ml-6">
           <li>Have a web application designed and made for you.</li>
           <li>Consulting on your web-based project.</li>
           <li>
             Crafting of a proper architecture diagram for your application.
           </li>
         </ul>
-        <p className="text-gray-700 mx-auto text-base font-body font-light leading-relaxed mb-6">
+        <p className="text-gray-700 mx-auto text-base font-body leading-relaxed mb-6">
           If you're interested in the services I have to offer you can contact
           me through my email:{" "}
           <a href="mailto:cjimenezber@gmail.com">cjimenezber@gmail.com</a>, you
