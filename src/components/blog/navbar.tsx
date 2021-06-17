c82cc1563c8b5bc49502d1b0586b176ac7e495ab
Carlos Jose Jimenez <cjimenez@deckprismsports.com>
2021-06-17 16:08:24 -0600

    Fix css.

--- src/components/blog/navbar.tsx
+++ src/components/blog/navbar.tsx
@@ -5,21 +5,23 @@ import { Link } from "gatsby";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="py-1 h-20 flex justify-around bg-red-900">
-      <Link to="/" className="my-auto">
-        <img src={InvertedLogo} className="h-16" />
-      </Link>
-      <div className="flex my-auto text-lg text-white font-semibold font-header">
-        <Link to="/" className="mx-3 shadow-none hover:text-red-400">
-          Home
+    <nav className="py-1 max-h-20 bg-red-900">
+      <div className="flex justify-around">
+        <Link to="/" className="my-auto">
+          <img src={InvertedLogo} className="h-16" />
         </Link>
-        <Link to="/latest" className="mx-3 shadow-none hover:text-red-400">
-          Blog
-        </Link>
-        <CategoryMenu />
-        <Link to="/contact" className="mx-3 shadow-none hover:text-red-400">
-          Hire Me
-        </Link>
+        <div className="flex my-auto text-lg text-white font-semibold font-header">
+          <Link to="/" className="mx-3 shadow-none hover:text-red-400">
+            Home
+          </Link>
+          <Link to="/latest" className="mx-3 shadow-none hover:text-red-400">
+            Blog
+          </Link>
+          <CategoryMenu />
+          <Link to="/contact" className="mx-3 shadow-none hover:text-red-400">
+            Hire Me
+          </Link>
+        </div>
       </div>
     </nav>
   );
