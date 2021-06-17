e5f9438e580fdf94adc308c83e003ba7833772c6
Carlos Jose Jimenez <cjimenez@deckprismsports.com>
2021-06-17 16:18:11 -0600

    Small change for GA.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,19 @@ module.exports = {
     categories: ["Web Development", "Cloud Architecture", "Product Management"]
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-VQNN2JYBF3", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      }
+    },
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-postcss",
     "gatsby-plugin-react-helmet",
@@ -51,25 +64,5 @@ module.exports = {
         policy: [{ userAgent: '*', allow: '/' }]
       }
     },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        trackingIds: [
-          "G-VQNN2JYBF3", // Google Analytics / GA
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: true,
-        },
-      }
-    }
   ],
 };
